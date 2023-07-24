0bbda5bc4cfacfa5f8c8ae380dbc6d945329a876
kaapyth0n <nikita.kapitonov@gmail.com>
2023-07-24 17:02:03 +0200

    24.07 добавили босса

--- pursuit-master-beta-master/main.py
+++ pursuit-master-beta-master/main.py
@@ -15,21 +15,7 @@ W,H = 700, 700
 TILE = 45
 GAME_RES = W * TILE, H * TILE
 
-# class Diamond(FloatLayout):
-#     def __init__(self, **kwargs):
-#         super(Diamond, self).__init__(**kwargs)
-#         self.image = Image(source = "res/diamond.png")
-#         self.add_widget(self.image)
-#         self.update_position()
-#     def update_position(self, *args):
-#         x = random.randint(40, 870)
-#         y = random.randint(210, 680)
-#         self.image.pos = (x, y)
 class GridGame(Widget):
-    # def download_player(self, wid, TILE_W, TILE_H):
-    #     with wid.canvas:
-    #         img = Image(source = "res/player.png", size_hint = (.5, .5), pos_hint = {"center_x": 50, "center_y": 50})
-    #         return img
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -86,18 +72,56 @@ class GridGame(Widget):
     def on_key_up(self, window, key, *args):
         self.key_pressed.remove(self.key_codes.get(key, ''))
 
+class Boss(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        self.add_widget(BoxLayout())
+        self.img = Image(source = "res/boss.png", pos = (870, 420))
+        self.add_widget(self.img)
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def pursuit(self, player):
+        dx = player.x - self.x
+        dy = player.y - self.y
+        distance = (dx**2 + dy**2)**0.5
+        if distance < 50:
+            self.velocity_x = dx / distance
+            self.velocity_y = dy / distance
+        else:
+            self.velocity_x = 0
+            self.velocity_y = 0
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+        if self.img.x == 870:
+            self.img.x -= 10
+        else:
+            self.img.x += self.speed
+        if self.img.x == 40:
+            self.img.x += 10
+        else:
+            self.img.x -= self.speed
+        if self.img.y == 210:
+            self.img.y += 10
+        else:
+            self.img.y -= self.speed
+        if self.img.y == 680:
+            self.img.y -= 10
+        else:
+            self.img.y += self.speed
+
+    def shoot(self):
+        pass
 
 class PursuitGame(Widget):
     my_grid = GridGame()
-    # my_diamond = Diamond()
+    my_boss = Boss()
 
 
 class PursuitApp(App):
     def build(self):
         Window.bind(on_resize = self.on_window_resize)
-        # diamond = Diamond()
-        # Clock.schedule_interval(diamond.update_position, 10)
-        # return diamond
         return PursuitGame()
     def on_start(self):
         self._width = self.root.ids["game_layout"].width
@@ -106,9 +130,6 @@ class PursuitApp(App):
         self._height_glass = self._height -40
         TILE_W = self._width_glass / W
         TILE_H = self._height_glass / H
-
-
-        # self.root.my_grid.download_player(self.root.ids["game_layout_box"], TILE_W, TILE_H)
 
     def on_window_resize(self, window, width, height):
         self._width = self.root.ids["game_layout"].width
